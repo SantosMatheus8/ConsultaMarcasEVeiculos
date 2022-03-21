3e8ffee801e1f5f15d9238b7d9ac27f9a3e2add2
Matheus Pereira <matheus.melo2008@hotmail.com>
2022-03-21 17:15:11 -0300

    Configurando para pegar modelos de carro e imprimir no console

--- JavaScript/API.js
+++ JavaScript/API.js
@@ -8,7 +8,6 @@ let listaMotos = document.querySelector(".listaMotos")
 let urlCaminhoes = `https://parallelum.com.br/fipe/api/v1/caminhoes/marcas`
 let listaCaminhoes = document.querySelector(".listaCaminhoes")
 
-
 function pegaApi(url)
 {
     let req = new XMLHttpRequest()
@@ -16,14 +15,12 @@ function pegaApi(url)
     req.send()
     return req.responseText
 }
-function adicionaLinha(marca,lista)
+function adicionaLinha(marca)
 {
     let iten = document.createElement("li")
-    let link = document.createElement("a")
-    link.href = "paginaModelos.html"
-    iten.appendChild(link)
-    link.innerHTML = marca.nome
-    lista.appendChild(iten)
+    iten.setAttribute("value", marca.codigo)
+    iten.addEventListener('click', () => escolheMarca(event))
+    iten.innerHTML = marca.nome        
 
     return iten
 }
@@ -35,19 +32,43 @@ function main(url,lista)
 
 
     marcas.forEach(element => {
-        let iten = adicionaLinha(element,lista)
-        // iten.classList.add(`${element.codigo}`)
+        let iten = adicionaLinha(element)
         lista.appendChild(iten)
     });
 }
 
+function escolheMarca(event)
+{
+    let marcaClicada = event.target.value
+
+    let elemento = event.target.parentNode
+    let nomeId = elemento.getAttribute("id") 
+    //nomeId --> É o nome do id do pai do elemento clicado, ex : carros
+    //marcaClicada --> pega o código da marca clicada
+
+     url = `https://parallelum.com.br/fipe/api/v1/${nomeId}/marcas/${marcaClicada}/modelos`
+
+
+     let data = pegaApi(url)
+     let marcas = JSON.parse(data)
+     let tam = marcas.modelos.length
+
+    for(let i = 0; i < tam; i++)
+    {
+        console.log(marcas.modelos[i].nome)
+    }
+
+
+}
+
+function aux(marcas)
+{
+    console.log(marcas.modelos[1].nome)
+    marcas.forEach(element => {
+       element.modelos
+    });
+}
 
 main(urlCarros, listaCarros)
 main(urlMotos, listaMotos)
 main(urlCaminhoes, listaCaminhoes)
-
-
-
-
-
-
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -48,7 +48,7 @@ function mudaCoreSetaCaminhao()
 }
 
 botaoCaminhao.addEventListener('click', () =>
-{
+{   
         listaCaminhao.classList.toggle('ativaAccordion')
 })
 
